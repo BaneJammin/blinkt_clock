394d7994b7a7687ca8b953d60c3a76a1af915dd8
BaneJammin <vivimemorleti@gmail.com>
2018-01-27 15:04:22 -0500

    blank lights are white for ease of use in darkness

--- blinkt_clock.py
+++ blinkt_clock.py
@@ -10,14 +10,16 @@ b = 64
 v = .05
 
 set_clear_on_exit(False)
-clear(); show()
+clear()
+set_all(25, 25, 35, v)
+show()
 
 split_time = strftime('%H %M %p').split(' ')
 time_hour = int(split_time[0])
 time_minute = int(split_time[1])
 
 #Set the Lo/Hi and AM/PM bits 
-if time_hour > 12:
+if  time_hour > 12:
     set_pixel(0, 0, g, 0, v)
 else:
     set_pixel(0, 0, 0, 0)
@@ -30,7 +32,7 @@ minute_pixel = int(round(time_minute, -1) / 10)
 hour_pixel = time_hour % 6 if time_hour % 6 > 0 else 6 
 
 if minute_pixel == 0: 
-    set_pixel(hour_pixel, 0, 0, b, v)
+    set_pixel(hour_pixel, 0, 0, b, v*2)
 elif minute_pixel != hour_pixel:
     set_pixel(minute_pixel, r, 0, 0, v)
     set_pixel(hour_pixel, 0, 0, b, v)
